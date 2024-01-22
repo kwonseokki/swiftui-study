619056a0a368108a41e7f8797af40b1683d022c8
KwonSeokki <kwons3327@gmail.com>
2024-01-22 22:25:11 +0900

    [Feat] 100DaysOfSwiftUI - Day20-22

--- 100daysOfSwiftUI/day20-22/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ 100daysOfSwiftUI/day20-22/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var shoingScore = false
     @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var numberOfGame = 0
     
     var body: some View {
         ZStack {
@@ -19,7 +21,7 @@ struct ContentView: View {
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
             ], center: .top, startRadius: 200, endRadius: 400)
-                .ignoresSafeArea()
+            .ignoresSafeArea()
             
             VStack(spacing: 15) {
                 VStack {
@@ -40,35 +42,47 @@ struct ContentView: View {
                             .shadow(radius: 5)
                     }
                     .alert(scoreTitle, isPresented: $shoingScore) {
-                        Button("Continue", action: askQuestion)
+                        if numberOfGame < 8 {
+                            Button("Continue", action: askQuestion)
+                        } else {
+                            Button("Reset", action: reset)
+                        }
                     } message: {
-                        Text("Your score is ???")
+                        Text("Your score is \(score)")
                     }
                 }
                 
                 
             }
             .frame(maxWidth: .infinity)
-                .padding(.vertical, 20)
-                .background(.regularMaterial)
-                .clipShape(.rect(cornerRadius: 20))
+            .padding(.vertical, 20)
+            .background(.regularMaterial)
+            .clipShape(.rect(cornerRadius: 20))
             .padding()
         }
     }
     
     func flagTapped(_ number: Int) {
+        
         if number == correctAnswer {
             scoreTitle = "Correct"
+            score += 1
         } else {
             scoreTitle = "Wrong"
         }
-        
+        numberOfGame += 1
         shoingScore = true
     }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func reset() {
+        score = 0
+        numberOfGame = 0
+        countries.shuffle()
     }
 }
 
